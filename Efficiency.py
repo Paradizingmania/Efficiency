0972a622487c25e4a1801141e53addf75b059a2d
Paradizingmania <Elias.a.kaerhoeg@gmail.com>
2018-12-19 21:33:18 +0100

    Final For SRP
    Sidste ændringer til srp

--- Efficiency.py
+++ Efficiency.py
@@ -1,15 +1,24 @@
+# created by Elias Axel Kærhøg
 import math
-# all values will be for little boy bomb with uranium core an Tungsten-carbide Tamper:
-# define constants for later use:
+# all values will be for little boy bomb with uranium core an Tungsten-carbide Tamper
+# calculated using equations by Reed
 
 
-def efficiency():
+def efficiency(mass_core, mass_tamper):
 
-    m_neutron = 1.674927 * 10 ** (- 27)  # neutron mass in kg
+    if mass_core == 0:
+        m_core = 64000  # mass of core in g 64000 g is default in little boy
+    else:
+        m_core = mass_core
+
+    if mass_tamper == 0:
+        m_tamper = 311000  # mass of tamper in g 311000 g is default in little boy
+    else:
+        m_tamper = mass_tamper
+
+    # define constants for later use:
     NA = 6.022142 * 10 ** 23  # Avogadro's number
     pi = math.pi
-    ep = 1.6021765 * 10 ** -13  # joule/MeV
-    E_neutron = 2  # energy of neutron in MeV
     v_neutron = 1.956083 * 10 ** 9  # neutron velocity in cm/s
     kt = 4.2 * 10 ** 12  # kiloton to joules
 
@@ -22,8 +31,6 @@ def efficiency():
     sig_elastic_core = 4.566  # cross section for elastic reaction in core in barns
     sig_elastic_tamper = 6.587  # cross section for elastic reaction in core in barns
     released_neutrons = 2.637  # average number of neutrons released during fission
-    m_core = 64000  # mass of core in kg 64000 kg is default in little boy
-    m_tamper = 552500  # mass of tamper in kg 552500 kg is default in little boy
     initial_compression = 1  # compression at start
     final_compression = 0.549927311 # final compression at k < 1
 
@@ -37,5 +44,27 @@ def efficiency():
     radius_start_core = ((3 * m_core) / (4 * pi * initial_compression * rho_core)) ** (1/3)
     radius_start_tamper = ((3 / (4 * pi)) * ((m_tamper / (initial_compression * rho_tamper)) + (m_core /
 (initial_compression * rho_core)))) ** (1/3)
-    d_core
+    d_core = 1 / ( 1 ** -24 * n_core * sig_core_total)
+    time_initial = (MFP_fission / v_neutron) / initial_compression
+
+    # calculate values for critical threshold
+    radius_final_core = ((3 * m_core)/(4 * pi * final_compression * rho_core)) ** (1/3)
+    radius_final_tamper =(( 3 / (4 * pi)) * ((m_tamper / (final_compression * rho_tamper)) + (m_core /
+(final_compression * rho_core)))) ** (1 / 3)
+    d_final = d_core / final_compression
+    MFP_tamper_final = MFP_transport_tamper / final_compression
+    MFP = MFP_tamper_final / MFP_transport_core
+    x = radius_final_core / d_final
+
+    delta_Radius = radius_final_core - radius_start_core
+    time_end = (MFP_fission / v_neutron) / final_compression
+    Yield = ((m_core + m_tamper) / 8000) * ((0.01 * delta_Radius * (math.log(released_neutrons)) / time_end) ** 2) / kt
+    eff = 100 * Yield / (17 * m_core/1000) # calculates efficiency of bomb
+
+    return (Yield)
+
+
+
+
+
 
--- Main.py
+++ Main.py
@@ -1,15 +1,140 @@
+# created by Elias Axel Kærhøg
+import csv
 import Efficiency
 while True:
-    print()
-    # efficiency = Efficiency.Efficiency()
-    # create csv file for results
-    i = 1
-    while True:
+
+    print("To calculate effect of tamper on yield press 1 to calculate effect of core mass on yield press 2")
+    selection = input()
+    if selection == "1":
+
+        print("select lower bound in grams")
+        lower_bound = input()
 
         try:
-            f = open("results_" + str(i) + ".csv", "x")  # check if result document exists
+            lower_bound = int(lower_bound)
+        except ValueError:
+            print("not an int")
             break
-        except (FileNotFoundError, FileExistsError):
-            i += 1
 
-    break
+        print("select upper bound in grams")
+        upper_bound = input()
+
+        try:
+            upper_bound = int(upper_bound)
+        except ValueError:
+            print("not an int")
+            pass
+
+        print("select interval in grams(standard is 1000)")
+        interval = input()
+
+        try:
+            interval = int(interval)
+        except ValueError:
+            print("not an int")
+            pass
+
+        print("select mass of core in grams, for default use 0")
+        core = input()
+
+        try:
+            core = int(core)
+        except ValueError:
+            print("not an int")
+            pass
+
+        i = 1
+        while True:
+
+            try:
+                f = open("results_varied_tamper" + str(i) + ".csv", "x")  # check if result document exists
+                break
+            except (FileNotFoundError, FileExistsError):
+                i += 1
+            break
+
+        j = 1
+        tamper = 0
+
+        while tamper <= upper_bound:
+            tamper = lower_bound + j * interval
+
+            print (str(Efficiency.efficiency(core, tamper)))
+
+            row = [core, tamper, str(Efficiency.efficiency(core,tamper))]
+
+            with open("results_varied_tamper"+str(i)+".csv", "a") as file:
+                writer = csv.writer(file)
+                writer.writerow(row)
+
+            j += 1
+
+        file.close()
+        break
+
+    if selection == "2":
+
+        print("select lower bound in grams")
+        lower_bound = input()
+
+        try:
+            lower_bound = int(lower_bound)
+        except ValueError:
+            print("not an int")
+            break
+
+        print("select upper bound in grams")
+        upper_bound = input()
+
+        try:
+            upper_bound = int(upper_bound)
+        except ValueError:
+            print("not an int")
+            pass
+
+        print("select interval in grams(standard is 1000)")
+        interval = input()
+
+        try:
+            interval = int(interval)
+        except ValueError:
+            print("not an int")
+            pass
+
+        print("select mass of tamper in grams, for default use 0")
+        tamper = input()
+
+        try:
+            tamper = int(tamper)
+        except ValueError:
+            print("not an int")
+            pass
+
+        i = 1
+        while True:
+
+            try:
+                f = open("results_varied_core" + str(i) + ".csv", "x")  # check if result document exists
+                break
+            except (FileNotFoundError, FileExistsError):
+                i += 1
+            break
+
+        j = 1
+        core = 0
+
+        while core <= upper_bound:
+            core = lower_bound + j * interval
+
+            print (str(Efficiency.efficiency(core, tamper)))
+
+            row = [core, tamper, str(Efficiency.efficiency(core,tamper))]
+
+            with open("results_varied_core"+str(i)+".csv", "a") as file:
+                writer = csv.writer(file)
+                writer.writerow(row)
+
+            j += 1
+
+        file.close()
+        break
